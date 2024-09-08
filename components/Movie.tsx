11e02d204405d75fbe72eea3ab96aac28936e42d
jaemin <als982001@naver.com>
2024-09-08 15:43:11 +0900

    prefetch 적용

--- components/Movie.tsx
+++ components/Movie.tsx
@@ -18,7 +18,9 @@ export default function Movie({ title, id, posterPath }: IMovieProps) {
   return (
     <div className={styles.movie}>
       <img src={posterPath} alt={title} onClick={onClick} />
-      <Link href={`/movies/${id}`}>{title}</Link>
+      <Link prefetch href={`/movies/${id}`}>
+        {title}
+      </Link>
     </div>
   );
 }
